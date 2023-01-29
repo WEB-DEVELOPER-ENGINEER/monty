4891a148a4be9b674acadf39a0527c001597f895
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-29 09:07:49 -0800

    done

--- functions2.c
+++ functions2.c
@@ -44,3 +44,82 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub - substract the top by the second top.
+ * @stack: Head of the doubly linked list.
+ * @line_number: Line num
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int res, i;
+
+	stack_t *tmp = *stack;
+
+	for (i = 0; tmp != NULL; i++)
+		tmp = tmp->next;
+	if (i < 2 || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	res = tmp->n - (*stack)->n;
+	free(*stack);
+	*stack = tmp;
+	(*stack)->n = res;
+}
+
+/**
+ * _div - divide the top by the second top.
+ * @stack: Head of the doubly linked list.
+ * @line_number: Line num
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int res;
+
+	stack_t *tmp = *stack;
+
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	res = tmp->n / (*stack)->n;
+	free(*stack);
+	*stack = tmp;
+	(*stack)->n = res;
+}
+
+/**
+ * _mul - multiplicate the top by the second top.
+ * @stack: Head of the doubly linked list.
+ * @line_number: The line to execute
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int res;
+
+	stack_t *tmp = *stack;
+
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	res = (*stack)->n * tmp->n;
+	free(*stack);
+	*stack = tmp;
+	(*stack)->n = res;
+}
